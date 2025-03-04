52727f4c2db329cd1deede37be2b50d1c96142a8
aaaaarohi <aarohi24@iiserb.ac.in>
2025-03-04 05:20:47 -0800

    Update numberplate_detection.py

---
+++ numberplate_detection.py
@@ -0,0 +1,33 @@
+import cv2
+
+harcascade = "indian_license_plate.xml"
+
+cap = cv2.VideoCapture(0)
+
+cap.set(3, 640) # width
+cap.set(4, 480) #height
+
+min_area = 500
+
+while True:
+    success, img = cap.read()
+
+    plate_cascade = cv2.CascadeClassifier(harcascade)
+    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    plates = plate_cascade.detectMultiScale(img_gray, 1.1, 4)
+
+    for (x,y,w,h) in plates:
+        area = w * h
+
+        if area > min_area:
+            cv2.rectangle(img, (x,y), (x+w, y+h), (0,255,0), 2)
+           
+           #cropping the number plate part and saving the plate area
+            img_roi = img[y: y+h, x:x+w]
+            #cv2.imshow("ROI", img_roi)
+
+
+    
+    cv2.imshow("Result", img)
+
