406adc44390bf7173dd7e4d2ecde640c94cae589
aaaaarohi <aarohi24@iiserb.ac.in>
2025-03-04 22:40:21 -0800

    Update store_time.py

--- store_time.py
+++ store_time.py
@@ -1,21 +1,19 @@
 import sqlite3
+import datetime
 from read_text import*
 from store_time import*
+from car_detection import*
 
 number_plate=extracted_text
+
+if is_detected:
+    detection_time = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
 
 # Connect to SQLite database (creates file if not exists)
 conn = sqlite3.connect("vehicles.db")
 cursor = conn.cursor()
 
-# Create a table for storing vehicle number plates
-cursor.execute('''
-    CREATE TABLE IF NOT EXISTS vehicles (
-        number_plate TEXT PRIMARY KEY,
-        in_time TEXT,
-        out_time TEXT
-    )
-''')
 
 cursor.execute("INSERT INTO vehicles (number_plate) VALUES (?)", (number_plate,))
 cursor.execute("INSERT INTO vehicles (in_time) VALUES (?)", (detection_time,))
